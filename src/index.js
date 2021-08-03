21383f5f88c25ffe65e377b3a5cf18b6a033fe1d
cjmaret <77926563+cjmaret@users.noreply.github.com>
2021-08-03 23:31:06 +0700

    change basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename='/'>
+    <HashRouter basename='/portfolio-react'>
       <App />
     </HashRouter>
   </React.StrictMode >,
