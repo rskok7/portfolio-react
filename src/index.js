e6350c9e637f307a82e6e6a6ee9df8247ce3779f
cjmaret <cjmaret@gmail.com>
2021-09-04 16:21:37 -0400

    refactor: change HashRouter to BrowserRouter, remove basename

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './components/App.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename='/portfolio-react'>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode >,
   document.getElementById('root')
 );
