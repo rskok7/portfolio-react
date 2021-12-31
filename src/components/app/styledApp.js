db300fe88f3e2d281aea0d379cdc10d288939fa1
cjmaret <cjmaret@gmail.com>
2021-12-31 21:27:10 +0700

    refactor: change dracula button

--- src/components/app/styledApp.js
+++ src/components/app/styledApp.js
@@ -5,24 +5,11 @@ export const AppContainer = styled.div`
   background: ${(props) => (props.draculaMode ? '#A0A9BA' : '')};
 `;
 
-
-export const DraculaButtonContainer = styled.div`
-  position: fixed;
-  width: 125px;
-  height: 150px;
-  top: 200px;
-  left: ${(props) => (props.isDraculaHovered ? '-10px' : '-45px')};
-  transition: all 0.4s ease;
-  display: flex;
-  align-items: center;
-  z-index: 3;
-`;
-
 export const EmojiSpan = styled.span`
   position: absolute;
   opacity: 1;
   transition: all 0.4s ease;
-  left: 9.5px;
+  left: 7px;
   top: 4px;
 `;
 
@@ -43,30 +30,46 @@ export const DraculaButton = styled.div`
   position: relative;
   color: white;
   height: 5px;
-  width: 15px;
-  left: 25px;
+  width: 35px;
+  right: 20px;
   padding: 10px 10px;
   background: ${(props) => (props.draculaMode ? '#892738' : 'grey')};
   border: 3px solid #2b303b;
   border-radius: 25px;
   transform: scale(1);
   transition: all 0.4s ease;
-  opacity: 0.4;
   user-select: none;
+  opacity: 0.8;
 
   &:hover {
     cursor: pointer;
     transform: scale(1.15);
-    height: 10px;
-    width: 100px;
-    opacity: 1;
   }
+`;
+
+export const DraculaButtonContainer = styled.div`
+  position: fixed;
+  width: 150px;
+  height: 150px;
+  top: 200px;
+  right: ${(props) => (props.isDraculaHovered ? '-5px' : '-50px')};
+  transition: all 0.4s ease;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  z-index: 3;
 
   &:hover ${EmojiSpan} {
     opacity: 0;
   }
 
   &:hover ${TextSpan} {
+    opacity: 1;
+  }
+
+  &:hover ${DraculaButton} {
+    height: 10px;
+    width: 100px;
     opacity: 1;
   }
 `;
