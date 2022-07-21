1ea655cf6ee9bb0cb28734f4051beb69cc61bd56
cjmaret <cjmaret@gmail.com>
2022-07-21 17:57:05 +0700

    feat: add historymuffin; fix: reduce project image file sizes

--- src/arrays/project-cards.js
+++ src/arrays/project-cards.js
@@ -7,10 +7,20 @@ import PlanetsReactImage from '../images/planets-react-images/planets-react-main
 import ApiaryHomepageImage from '../images/apiary-images/apiary-homepage.png';
 import NewsExplorerHomepageImage from '../images/news-explorer-images/news-explorer-homepage.png';
 import DotcotHomepageImage from '../images/dotcot-images/dotcot-homepage.png';
-import ConfectioneryImage from '../images/confectionery-images/confectionery-2.png'
+import ConfectioneryImage from '../images/confectionery-images/confectionery-homepage.png';
+import HistoryMuffinImage from '../images/history-muffin-images/history-muffin-homepage.png'
 
 const projectCards = [
-    {
+  {
+    id: 11,
+    image: HistoryMuffinImage,
+    alt: 'History Muffin E-commerce store',
+    description:
+      'An e-commerce store where users can purchase or sell products, create an account, manage their products, and make purchases using Stripe',
+    pageLink: 'https://historymuffin.com',
+    githubLink: 'https://github.com/cjmaret/history-muffin-frontend',
+  },
+  {
     id: 10,
     image: ConfectioneryImage,
     alt: 'The Confectionery Homepage',
